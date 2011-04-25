03b6f5a498678d951caa490690a4dfffe1e8500b
Jahrome <jahrome11@gmail.com>
2011-04-25 12:17:15 +0200

    Android build

---
+++ Android.mk
@@ -0,0 +1,21 @@
+LOCAL_PATH := $(call my-dir)
+
+LIB_PATH := build/ffmpeg/lib
+BIN_PATH := build/ffmpeg/bin
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS:=eng debug
+
+LOCAL_PREBUILT_LIBS := \
+	$(LIB_PATH)/libavformat.so \
+	$(LIB_PATH)/libavutil.so \
+	$(LIB_PATH)/libswscale.so \
+	$(LIB_PATH)/libavcodec.so
+
+LOCAL_PREBUILT_EXECUTABLES := \
+	$(BIN_PATH)/ffmpeg \
+	$(BIN_PATH)/ffprobe \
+	$(BIN_PATH)/ffserver
+
+include $(BUILD_MULTI_PREBUILT)
---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+export NDK=${HOME}/Projet_android/android-ndk-r5
+export TOOLCHAIN=${HOME}/standalone-toolchain
+#$NDK/build/tools/make-standalone-toolchain.sh --platform=android-9 --install-dir=$TOOLCHAIN
+export PATH=$TOOLCHAIN/bin/:$PATH
+
+EXTRA_CFLAGS="-march=armv7-a -mfloat-abi=softfp -mfpu=neon"
+EXTRA_LDFLAGS="-Wl,--fix-cortex-a8"
+EXTRA_LIBS="-lgcc"
+
+FLAGS="--target-os=linux --cross-prefix=arm-linux-androideabi- --arch=arm"
+FLAGS="$FLAGS --prefix=build/ffmpeg"
+FLAGS="$FLAGS --disable-static --enable-shared --enable-postproc --disable-symver --enable-gpl --enable-nonfree"
+#FLAGS="$FLAGS --disable-encoders --disable-decoders --disable-protocols --disable-muxers --disable-demuxers --disable-parsers --disable-devices --disable-filters --disable-bsfs"
+FLAGS="$FLAGS --enable-encoder=mpeg2video"
+
+rm -rf build/ffmpeg
+mkdir -p build/ffmpeg
+echo $FLAGS --extra-cflags="$EXTRA_CFLAGS" --extra-ldflags="$EXTRA_LDFLAGS" --extra-libs="$EXTRA_LIBS" > build/ffmpeg/info.txt
+./configure $FLAGS --extra-cflags="$EXTRA_CFLAGS" --extra-ldflags="$EXTRA_LDFLAGS" --extra-libs="$EXTRA_LIBS" | tee build/ffmpeg/configuration.txt
+[ $PIPESTATUS == 0 ] || exit 1
+make clean
+make -j2 || exit 1
+make install || exit 1
